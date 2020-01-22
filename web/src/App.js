300cbb9f9a41bc28afe10f7c312a1ae3e60cc871
lopessoftec <lopessoftec@gmail.com>
2020-01-21 23:28:12 -0300

    finalizado a parte web do projeto radar

--- web/src/App.js
+++ web/src/App.js
@@ -1,85 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+// useEffect serve para disparamos uma função, toda vex que uma informação alterar ou uma unica vez
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 function App() {
+  const [devs, setDevs] = useState([]); //ja que é multiplos devs é bom array
+
+  useEffect(() => {
+    async function loadDevs() {
+
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    //e.preventDefault(); //rvitar enviar para outra página
+
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data]); //é assim que faz uma adição. remover o .filter, alterar .map
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required/>
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/>
-            </div>
-          </div>
-          <button type="submit">Salvar</button>        
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev_item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/54129629?s=460&v=4" alt="Ytalo Lopes"/>
-              <div className="user-info">
-                <strong>Ytalo Lopes</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>CEO na Lopessoftec. Desenvolvedor web em PHP e aprendendo Javascript, reactJs, React Native, NodeJS,... Sempre em busca de novos conhecimentos.</p>
-            <a href="https://github.com/lopessoftec">Acessar perfil no Github</a>
-          </li>
-          <li className="dev_item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/54129629?s=460&v=4" alt="Ytalo Lopes"/>
-              <div className="user-info">
-                <strong>Ytalo Lopes</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>CEO na Lopessoftec. Desenvolvedor web em PHP e aprendendo Javascript, reactJs, React Native, NodeJS,... Sempre em busca de novos conhecimentos.</p>
-            <a href="https://github.com/lopessoftec">Acessar perfil no Github</a>
-          </li>
-          <li className="dev_item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/54129629?s=460&v=4" alt="Ytalo Lopes"/>
-              <div className="user-info">
-                <strong>Ytalo Lopes</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>CEO na Lopessoftec. Desenvolvedor web em PHP e aprendendo Javascript, reactJs, React Native, NodeJS,... Sempre em busca de novos conhecimentos.</p>
-            <a href="https://github.com/lopessoftec">Acessar perfil no Github</a>
-          </li>
-          <li className="dev_item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/54129629?s=460&v=4" alt="Ytalo Lopes"/>
-              <div className="user-info">
-                <strong>Ytalo Lopes</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>CEO na Lopessoftec. Desenvolvedor web em PHP e aprendendo Javascript, reactJs, React Native, NodeJS,... Sempre em busca de novos conhecimentos.</p>
-            <a href="https://github.com/lopessoftec">Acessar perfil no Github</a>
-          </li>
+          {/* não coloco chaves prque estou declarando o retiorno da função */}
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>
